610f896be0d1df24bb265fca4e3f90d74c0bfa78
somashekhar <somashekharm4159@gmail.com>
2025-02-10 14:06:02 +0530

    SwapTwoNumbers

---
+++ numberSeries/08/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+//8. WAP to swap 2 numbers with only 2 variables
+func main() {
+	SwapTwoNumbers(23, -89)
+}
+
+func SwapTwoNumbers(num1, num2 int) {
+	fmt.Printf("num1 : %d and num2 : %d\n", num1, num2)
+	num1 = num1 + num2
+	num2 = num1 - num2
+	num1 = num1 - num2
+	fmt.Printf("num1 : %d and num2 : %d\n", num1, num2)
+}
